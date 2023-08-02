91eb9243fe9523582ea939ffd7de1b195484a491
metrictulip0309 <eclairez995@gmail.com>
2023-08-02 17:41:52 -0400

    fizz buzz assignment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,18 @@
-# add your code here
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range (101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif i % 3 == 0:
+        print("fizz")
+        continue
+    elif i % 5 == 0:
+        print("buzz")
+        continue
+    print(i)
 
